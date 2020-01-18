e3fdfc04292bef17a9a079c1db417431c42283a3
Armando Santos <adavid988@gmail.com>
2020-01-18 14:30:10 +0000

    article & sections sizes -styles / form partial & styles / form button -style

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,7 +1,14 @@
-// Change Menu Icon into an "X"
+// Change Menu Icon into an "X", Show Nav & Hide Nav after a link click;
 const menuIcon = document.querySelector("#menu_icon");
 const mainNav = document.querySelector("#main_nav");
+const navLinks = document.querySelectorAll("li");
 
 menuIcon.addEventListener("click", () => {
   mainNav.classList.toggle("nav_show");
+});
+
+navLinks.forEach(link => {
+  link.addEventListener("click", () => {
+    mainNav.classList.remove("nav_show");
+  });
 });
