0fceff583ab0a7de271fe7461b65927d88c18bdf
Armando Santos <adavid988@gmail.com>
2020-01-18 18:36:58 +0000

    branding transition, nav items transition delay

--- dist/js/main.js
+++ dist/js/main.js
@@ -12,3 +12,14 @@ navLinks.forEach(link => {
     mainNav.classList.remove("nav_show");
   });
 });
+
+// Hide brading on scroll down & show on scroll up
+const branding = document.querySelector("#branding");
+
+window.addEventListener("scroll", () => {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    branding.classList.add("hide");
+  } else {
+    branding.classList.remove("hide");
+  }
+});
